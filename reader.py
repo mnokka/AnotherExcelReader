78a09a26eaf74ea89497dcdb95760214bc42b175
Mika Nokka <mika.nokka@ambientia.fi>
2018-04-19 11:50:46 +0300

    Print all keys and their contents

--- reader.py
+++ reader.py
@@ -84,12 +84,14 @@ def Parse(filepath, filename):
     logging.debug ("CurrentSheet:{0}".format(CurrentSheet))
     logging.debug ("First row:{0}".format(CurrentSheet['A4'].value))
 
+    #CONFIGURATIONS
     DATASTARTSROW=5 # data section starting line
     K=11
+    G=6
     #for cell in CurrentSheet['A']:
     #    logging.debug  ("Row value:{0}".format(cell.value))
     # EXCEL COLUMN MAPPINGS:
-    # B=KEY, K=LINKED_ISSUES (11)
+    # B=KEY, K=LINKED_ISSUES (11), G=REPORTER (6)
     
     
     mylist = []
@@ -100,22 +102,29 @@ def Parse(filepath, filename):
             mylist.append(mycell.value)
             KEY=mycell.value
             logging.debug("ROW:{0} Original ID:{1}".format(i,mycell.value))
-            #Issues[KEY]=KEY # add to dictionary as master key (KEY)
+            Issues[KEY]={} # add to dictionary as master key (KEY)
             
             LINKED_ISSUES=(CurrentSheet.cell(row=i, column=11).value)
             logging.debug("Attachment:{0}".format((CurrentSheet.cell(row=i, column=K).value))) # for the same row, show also column K (LINKED_ISSUES) values
             Issues[KEY]["LINKED_ISSUES"] = LINKED_ISSUES
+            
+            REPORTER=(CurrentSheet.cell(row=i, column=11).value)
+            logging.debug("Attachment:{0}".format((CurrentSheet.cell(row=i, column=G).value))) # for the same row, show also column K (LINKED_ISSUES) values
+            Issues[KEY]["REPORTER"] = REPORTER
+            
             logging.debug("---------------------------------------------------")
             i=i+1
-    print mylist # could collect all needed values to data structure
+    #print mylist # could collect all needed values to data structure
     print "--------------"
-    print Issues
+    #print Issues
     print Issues.items() 
     key=18503
     if key in Issues:
         print "EXISTS"
     else:
         print "NOT THERE"
+    for key, value in Issues.iteritems() :
+        print key, value
     #print((CurrentSheet.cell(row=4, column=3)).value) #K=11
     
 logging.debug ("--Python exiting--")
