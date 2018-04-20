a90d0a7aa18b75b86ff51a17db61e35a77e8ae04
Mika Nokka <mika.nokka@ambientia.fi>
2018-04-20 16:55:40 +0300

    Another sheet reading works

--- reader.py
+++ reader.py
@@ -64,25 +64,26 @@ def main(argv):
     Parse(filepath, filename)
 
 
-
+############################################################################################################################################
+#
 def Parse(filepath, filename):
     logging.debug ("Filepath: %s     Filename:%s" %(filepath ,filename))
     files=filepath+"/"+filename
     logging.debug ("File:{0}".format(files))
    
     Issues=defaultdict(dict) 
-    Data=defaultdict(dict) 
+
     
     MainSheet="general_report" # hardcoded for main issues?
     
     wb= openpyxl.load_workbook(files)
     types=type(wb)
-    logging.debug ("Type:{0}".format(types))
+    #logging.debug ("Type:{0}".format(types))
     sheets=wb.get_sheet_names()
-    logging.debug ("Sheets:{0}".format(sheets))
+    #logging.debug ("Sheets:{0}".format(sheets))
     CurrentSheet=wb[MainSheet] 
-    logging.debug ("CurrentSheet:{0}".format(CurrentSheet))
-    logging.debug ("First row:{0}".format(CurrentSheet['A4'].value))
+    #logging.debug ("CurrentSheet:{0}".format(CurrentSheet))
+    #logging.debug ("First row:{0}".format(CurrentSheet['A4'].value))
 
     #CONFIGURATIONS
     DATASTARTSROW=5 # data section starting line
@@ -94,17 +95,16 @@ def Parse(filepath, filename):
     #    logging.debug  ("Row value:{0}".format(cell.value))
   
     
-    mylist = []
-
+    ##############################################################################################
+    #Go through main excel sheet for main issue keys (and contents findings)
     i=DATASTARTSROW # brute force row indexing
     for row in CurrentSheet[('B{}:B{}'.format(DATASTARTSROW,CurrentSheet.max_row))]:  # go trough all column B (KEY) rows
         for mycell in row:
-            #mylist.append(mycell.value)
             KEY=mycell.value
             logging.debug("ROW:{0} Original ID:{1}".format(i,mycell.value))
             Issues[KEY]={} # add to dictionary as master key (KEY)
             
-            LINKED_ISSUES=(CurrentSheet.cell(row=i, column=K).value)
+            LINKED_ISSUES=(CurrentSheet.cell(row=i, column=K).value) #NOTE THIS APPROACH GOES ALWAYS TO THE FIRST SHEET
             #logging.debug("Attachment:{0}".format((CurrentSheet.cell(row=i, column=K).value))) # for the same row, show also column K (LINKED_ISSUES) values
             Issues[KEY]["LINKED_ISSUES"] = LINKED_ISSUES
             
@@ -116,7 +116,6 @@ def Parse(filepath, filename):
             
             logging.debug("---------------------------------------------------")
             i=i+1
-    #print mylist # could collect all needed values to data structure
     #print Issues
     print Issues.items() 
     key=18503 # check if this key exists
@@ -126,7 +125,50 @@ def Parse(filepath, filename):
         print "NOT THERE"
     for key, value in Issues.iteritems() :
         print key, value
-    #print((CurrentSheet.cell(row=4, column=3)).value) #K=11
+
+    ############################################################################################################################
+    # Check any remarks (subtasks) for main issue
+    
+    RemarksSheet="Tabelle2" # hardcoded for main issues?
+    SubSheet1=wb[RemarksSheet]
+
+    
+    # Find KTR keyword, after which subtasks are defined
+    i=1
+    for row in SubSheet1[('A{}:A{}'.format(SubSheet1.min_row,SubSheet1.max_row))]:  # go trough all column B (KEY) rows    
+        for mycell in row:
+            TMP=mycell.value
+            #logging.debug("ROW:{0} Value:{1}".format(i,mycell.value))
+            if TMP=="KTR":
+                DATASTARTSROW=i+1 # this line includes first subtask definition
+                break # TODO fix logic
+            i=i+1
+    
+            
+    i=DATASTARTSROW # brute force row indexing
+    for row in SubSheet1[('B{}:B{}'.format(DATASTARTSROW,SubSheet1.max_row))]:  # go trough all column B (KEY) rows
+        for mycell in row:
+            KEY=mycell.value
+            logging.debug("ROW:{0} Original ID:{1}".format(i,KEY))
+            if KEY in Issues: 
+              
+                print "Subtask has a known parent."
+                #BGR=(SubSheet1.cell(row=i, column=J).value) # This approach takes always values from the first sheet of excel 
+                REMARKKEY=SubSheet1['J{0}'.format(i)].value  # column J holds BGR numbers
+                Issues[KEY]["REMARKS"]={}
+                Issues[KEY]["REMARKS"][REMARKKEY] = {}
+                
+                DECK=SubSheet1['S{0}'.format(i)].value  # column S holds BGR numbers
+                Issues[KEY]["REMARKS"][REMARKKEY]["DECK"] = DECK
+                logging.debug("i:{0} DECK:{1} REMARKKEY:{2}".format(i,DECK,REMARKKEY))
+            else:
+                print "Error: Unknown parent found"
+        print "----------------------------------"
+        i=i+1
+
+    for key, value in Issues.iteritems() :
+        print key, value
+
     
 logging.debug ("--Python exiting--")
 if __name__ == "__main__":
